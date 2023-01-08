7f1e1b84708a70ea8d2c1613b54e5aec5e73e31f
Colin Stananrd <colinjstannard@gmail.com>
2023-01-08 14:43:12 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -366,7 +366,7 @@ function pieChart(diameter, data) {
   stroke(244);
   var lastAngle = 0;
   for (var i = 0; i < data.length; i++) {
-    if(i == selectedChart && inc == 0){
+    if(i == 20 - selectedChart && inc == 0){
       fill(50);
     } else if(i % 2 == 0){
       fill(17);
